f8444dcc21442277eacee1c1596d8e21325b1195
アモンナン <165276621+amornnan19@users.noreply.github.com>
2026-05-30 03:59:31 +0700

    feat: remove redundant Sign in heading on login pages

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Auth\Login;
 use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -29,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->login(Login::class)
             ->font('Space Grotesk')
             ->defaultThemeMode(ThemeMode::Dark)
             ->colors([
--- app/Providers/Filament/EmployeePanelProvider.php
+++ app/Providers/Filament/EmployeePanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Auth\Login;
 use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -27,7 +28,7 @@ class EmployeePanelProvider extends PanelProvider
         return $panel
             ->id('employee')
             ->path('portal')
-            ->login()
+            ->login(Login::class)
             ->font('Space Grotesk')
             ->defaultThemeMode(ThemeMode::Dark)
             ->colors([
